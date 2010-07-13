1f6e6aa624bd95078c26f21a6ea757e2e00fd285
Roman Kharitonov <roman@linux-ohh0.site>
2010-07-13 11:58:35 +1100

    11

--- stuff/fixperm.sh
+++ stuff/fixperm.sh
@@ -3,4 +3,5 @@ if [ $(hostname) = "Vortigaunt" ]; then
     IAM="/var/www/localhost/htdocs/iam";
 else
     IAM="/srv/www/vhosts/iam";
-sudo chmod 775 -R $IAM && sudo chgrp wheel -R $IAM
+sudo chmod 775 -R $IAM 
+sudo chgrp wheel -R $IAM
