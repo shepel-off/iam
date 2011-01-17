75f7e5045a60febeb4e5dd94544b4137f2b1bf05
Roman Kharitonov <refaim.vl@gmail.com>
2011-01-17 13:34:07 +1000

    Remove trailing spaces, convert tabs to spaces, convert CRLF to LF

--- urls.py
+++ urls.py
@@ -12,16 +12,17 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # Uncomment the next line to enable the admin:
-     url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/news/'}, name='iam-root'),
-     url(r'^login/', 'iam.authentication.views.login', name='iam-login'),
-     url(r'^logout/', 'django.contrib.auth.views.logout', name='iam-logout'),
-     (r'^admin/', include(admin.site.urls)),
-     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-     (r'^users/', include('iam.users.urls')),
-     (r'^news/', include('iam.news.urls')),
-     (r'^labs/', include('iam.labs.urls')),
-     (r'^publications/', include('iam.publications.urls')),
-     (r'^tinymce/', include('tinymce.urls')),
+    url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/news/'}, name='iam-root'),
+    url(r'^login/', 'iam.authentication.views.login', name='iam-login'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', name='iam-logout'),
+    (r'^admin/filebrowser/', include('filebrowser.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^users/', include('iam.users.urls')),
+    (r'^news/', include('iam.news.urls')),
+    (r'^labs/', include('iam.labs.urls')),
+    (r'^publications/', include('iam.publications.urls')),
+    (r'^tinymce/', include('tinymce.urls')),
 )
 
 from django.conf import settings
