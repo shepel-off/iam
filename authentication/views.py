c53a6a39179df5a5741c0cfa11d5277485219445
Roman Kharitonov <refaim.vl@gmail.com>
2011-01-18 12:36:44 +1000

    Add missing parameter

--- authentication/views.py
+++ authentication/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login as django_login
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
-def answer(**kwargs):
+def answer(request, **kwargs):
     args = { 'form': AuthenticationForm() }
     args.update(kwargs)
     return direct_to_template(request, 'registration/login.html', args)
@@ -20,9 +20,9 @@ def login(request):
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is None:
-            return answer(errors=[u'Неверный логин или пароль'])
+            return answer(request, errors=[u'Неверный логин или пароль'])
         elif not user.is_active:
-            return answer(errors=[u'Ваша учётная запись заблокирована'])
+            return answer(request, errors=[u'Ваша учётная запись заблокирована'])
         django_login(request, user)
         return redirect(request.POST['next'])
-    return answer(next=request.GET.get('next', '/'))
+    return answer(request, next=request.GET.get('next', '/'))
