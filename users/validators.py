587b683b9e4ee899228d6b2adf9bde3c40dbfd79
Roman Kharitonov <refaim.vl@gmail.com>
2011-01-18 12:38:26 +1000

    Remove obsolete files

--- users/validators.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.core.exceptions import ValidationError
-
-def not_empty_factory(msg):
-    def not_empty(value):
-        if len(value) == 0:
-            raise ValidationError(u"%s не есть может пусто" % msg)
-    return not_empty
